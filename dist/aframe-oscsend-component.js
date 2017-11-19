7eccccec501d8a52a30782fc27c0071c4dd2ef8c
rm8x <ryanmoore2@cmail.carleton.ca>
2017-11-19 16:07:44 -0500

    Update dist

--- dist/aframe-oscsend-component.js
+++ dist/aframe-oscsend-component.js
@@ -1,163 +1,186 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-
+/******/ 		module.l = true;
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	var OSC = window.OSC;
-	var osc = '';
+"use strict";
 
-	/* global AFRAME */
-	if (typeof AFRAME === 'undefined') {
-	  throw new Error('Component attempted to register before AFRAME was available.');
-	}
 
-	/**
-	 * OSCtest component for A-Frame.
-	 */
-	AFRAME.registerComponent('oscsend', {
-	  schema: {
-	    serverURL: {default: 'localhost'},
-	    serverPort: {default: 8080},
-	    messagePath: {default: ''},
-	  },
+var OSC = window.OSC;
+var osc = '';
 
-	  /**
-	   * Set if component needs multiple instancing.
-	   */
-	  multiple: true,
+/* global AFRAME */
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
 
-	  /**
-	   * Initialize OSC event listeners.
-	   */
-	  initOscListeners: function () {
-	    osc.on('open', () => {
-	      console.log('osc connection open');
-	    });
-	    
-	    osc.on('close', () => {
-	      console.log('osc connection closed');
-	    });
-	    
-	    osc.on('error', (err) => {
-	      console.log('osc error', err);
-	    });
-	  },
+/**
+ * OSCtest component for A-Frame.
+ */
+AFRAME.registerComponent('oscsend', {
+  schema: {
+    serverURL: { default: 'localhost' },
+    serverPort: { default: 8080 },
+    messagePath: { default: '' }
+  },
 
-	  messages: {
-	    positionMessage: new OSC.Message(['{replaceme}', 'position']),
-	    rotationMessage: new OSC.Message(['{replaceme}', 'rotation']),
-	  },
+  /**
+   * Set if component needs multiple instancing.
+   */
+  multiple: true,
 
-	  initReusedMessages: function () {
-	    for(v in this.messages) {
-	      this.messages[v].address = this.messages[v].address.replace('{replaceme}', this.data.messagePath || '');
-	      this.messages[v].types = 'fff';
-	      this.messages[v].args.push(0, 0, 0);
-	    }
-	  },
+  /**
+   * Initialize OSC event listeners.
+   */
+  initOscListeners: function initOscListeners() {
+    osc.on('open', function () {
+      console.log('osc connection open');
+    });
 
-	  /**
-	   * Called once when component is attached. Generally for initial setup.
-	   */
-	  init: function () {
-	    this.initReusedMessages = AFRAME.utils.bind(this.initReusedMessages, this);
-	    this.updateMessageArgs = AFRAME.utils.bind(this.updateMessageArgs, this);
+    osc.on('close', function () {
+      console.log('osc connection closed');
+    });
 
-	    var plugin = new OSC.WebsocketClientPlugin({host: this.data.serverURL, port: this.data.serverPort });
-	    osc = new OSC({ plugin: plugin });
+    osc.on('error', function (err) {
+      console.log('osc error', err);
+    });
+  },
 
-	    this.initOscListeners();
-	    this.initReusedMessages();
-	    osc.open();    
-	   },
+  messages: {
+    positionMessage: new OSC.Message(['{replaceme}', 'position']),
+    rotationMessage: new OSC.Message(['{replaceme}', 'rotation'])
+  },
 
-	  /**
-	   * Called when component is attached and when component data changes.
-	   * Generally modifies the entity based on the data.
-	   */
-	  update: function (oldData) {
-	   },
+  initReusedMessages: function initReusedMessages() {
+    for (v in this.messages) {
+      this.messages[v].address = this.messages[v].address.replace('{replaceme}', this.data.messagePath || '');
+      this.messages[v].types = 'fff';
+      this.messages[v].args.push(0, 0, 0);
+    }
+  },
 
-	  /**
-	   * Called when a component is removed (e.g., via removeAttribute).
-	   * Generally undoes all modifications to the entity.
-	   */
-	  remove: function () {
-	    osc.close();
-	   },
+  /**
+   * Called once when component is attached. Generally for initial setup.
+   */
+  init: function init() {
+    this.initReusedMessages = AFRAME.utils.bind(this.initReusedMessages, this);
+    this.updateMessageArgs = AFRAME.utils.bind(this.updateMessageArgs, this);
 
-	  /**
-	   * Called when entity pauses.
-	   * Use to stop or remove any dynamic or background behavior such as events.
-	   */
-	  pause: function () { },
+    osc = new OSC(); // defaults to WebsocketClientPlugin
 
-	  /**
-	   * Called when entity resumes.
-	   * Use to continue or add any dynamic or background behavior such as events.
-	   */
-	  play: function () { },
-	 
-	  //TODO: investigate AFRAME.utils.throttleTick for use here
-	  tick: function () {
-	    if (osc.status() !== OSC.STATUS.IS_OPEN) {
-	      return;
-	    }
-	    var el = this.el;
-	    this.updateMessageArgs(this.messages.rotationMessage, el.getAttribute('rotation'));
-	    this.updateMessageArgs(this.messages.positionMessage, el.getAttribute('position'));
-	    osc.send(this.messages.rotationMessage);
-	    osc.send(this.messages.positionMessage);
-	  },
+    this.initOscListeners();
+    this.initReusedMessages();
+    osc.open({ host: this.data.serverURL, port: this.data.serverPort });
+  },
 
-	  updateMessageArgs: function(message, xyzElAttribute) {
-	    message.args[0] = xyzElAttribute.x;
-	    message.args[1] = xyzElAttribute.y;
-	    message.args[2] = xyzElAttribute.z;
-	  }
-	});
+  /**
+   * Called when component is attached and when component data changes.
+   * Generally modifies the entity based on the data.
+   */
+  update: function update(oldData) {},
 
+  /**
+   * Called when a component is removed (e.g., via removeAttribute).
+   * Generally undoes all modifications to the entity.
+   */
+  remove: function remove() {
+    osc.close();
+  },
+
+  /**
+   * Called when entity pauses.
+   * Use to stop or remove any dynamic or background behavior such as events.
+   */
+  pause: function pause() {},
+
+  /**
+   * Called when entity resumes.
+   * Use to continue or add any dynamic or background behavior such as events.
+   */
+  play: function play() {},
+
+  //TODO: investigate AFRAME.utils.throttleTick for use here
+  tick: function tick() {
+    if (osc.status() !== OSC.STATUS.IS_OPEN) {
+      return;
+    }
+    var el = this.el;
+    this.updateMessageArgs(this.messages.rotationMessage, el.getAttribute('rotation'));
+    this.updateMessageArgs(this.messages.positionMessage, el.getAttribute('position'));
+    osc.send(this.messages.rotationMessage);
+    osc.send(this.messages.positionMessage);
+  },
+
+  updateMessageArgs: function updateMessageArgs(message, xyzElAttribute) {
+    message.args[0] = xyzElAttribute.x;
+    message.args[1] = xyzElAttribute.y;
+    message.args[2] = xyzElAttribute.z;
+  }
+});
 
 /***/ })
 /******/ ]);
