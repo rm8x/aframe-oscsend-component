9f3e2c9c7264d77073f93ed949d15647faa8c8e9
rm8x <ryanmoore2@cmail.carleton.ca>
2017-11-19 16:08:54 -0500

    add webpack exclude rule

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
         rules: [
         {
             test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
             use: {
                 loader: 'babel-loader',
                 options: {
